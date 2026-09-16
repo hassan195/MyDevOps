f4b1947a1988f371bb0832e6f437e2549a4db448
hassan195 <hassan195@gmail.com>
2026-09-16 19:26:43 +1000

    fix: suppress false positive from Bandit scan: #B104

--- task/week4/inventory-api/inventory-api.py
+++ task/week4/inventory-api/inventory-api.py
@@ -148,6 +148,7 @@ class Handler(BaseHTTPRequestHandler):
 # 4. 启动 HTTP 服务器
 # ==========================================
 if __name__ == "__main__":
+    # nosec B104 : [B104:hardcoded_bind_all_interfaces] Possible binding to all interfaces.
     server_address = ("0.0.0.0",3000 )
     httpd = HTTPServer(server_address, Handler)
     logging.info(f"Server running on http://localhost:{server_address[1]}")
